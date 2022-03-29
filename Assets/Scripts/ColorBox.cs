b66724335e20c19a0d89729d0250c877d0b95ccc
Jonatas Santos Da Silva <21502275@anhembimorumbi.edu.br>
2022-03-29 14:08:35 -0300

    Ajustes no boxColor

--- Assets/Scripts/ColorBox.cs
+++ Assets/Scripts/ColorBox.cs
@@ -8,7 +8,12 @@ using UnityEngine;
 public class ColorBox : MonoBehaviour
 {
     [SerializeField] SpriteRenderer renderer;
+
+    // Tempo que leva para trocar de cor
     [SerializeField] float timer = 0.1f;
+
+    // Stop serve para controlar quando o bloco deve parar de piscar
+    [SerializeField] bool stop = false;
 
     // Checa quanto tempo passou desde a última vez que a cor foi trocada
     private float timeChecker = 0;
@@ -33,13 +38,27 @@ public class ColorBox : MonoBehaviour
         timeChecker += Time.deltaTime;
         if (renderer != null && timeChecker >= timer)
         {
-            Color newColor = new Color(
-                Random.value,
-                Random.value,
-                Random.value
-            );
+            //Array de cores que vai de 0 a 5
+            Color[] colors = new Color[6];
 
-            renderer.color = newColor;
+            // Váriavel color que armazena a cor final, para que essa possa ser passada para o personagem
+            Color finalColor;
+
+            // Aqui é onde se adiciona as cores
+            colors[0] = Color.blue;
+            colors[1] = Color.red;
+            colors[2] = Color.green;
+            colors[3] = Color.cyan;
+            colors[4] = Color.magenta;
+            colors[5] = Color.yellow;
+
+
+            if(!stop)
+                renderer.color = colors[Random.Range(0, colors.Length)];
+            else{
+                finalColor = renderer.color;
+            }
+            
             timeChecker = 0f;
         }
     }
